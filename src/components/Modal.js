77f73ad7bf11a612e621fc96494f4f362ed575fa
Dymic Digital <tech@dymic.com>
2019-07-01 12:47:14 -0700

    update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,10 +12,10 @@ class Modal extends Component {
                 <div id='modal'>
                     <h2>Coming Soon!</h2>
                     <p>Thank you for expressing your interest in {this.props.children}! We are putting the finishing touches on {this.props.children} and you'll be the first to know when it's done.</p>
-                    <form method="POST" action='https://www.harmonyfamilyyoga.com/' name="harmonyfamilyyoga" data-netlify="true">
+                    <form method="POST" action='/' name="harmonyfamilyyoga" data-netlify="true">
+                      <input type="hidden" name="harmonyfamilyyoga" value="harmonyfamilyyoga" />
                       <input className="input" name='email' type="email" placeholder="email"/>
                       <button className='button small' type='submit' name='button'>Submit</button>
-                      <input type="hidden" name="harmonyfamilyyoga" value="harmonyfamilyyoga" />
                     </form>
                     <button className='button medium' onClick={this.props.onClose}>Close</button>
                 </div>
